58047655807a37bdd692ca9a7661efaf03ff003f
Dipen Bhanushali <dipen.bhanushali@gmail.com>
2013-07-06 00:11:03 +0530

    adding micropost list

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -13,7 +13,6 @@ require 'spec_helper'
 
 describe Micropost do
   let(:user) { FactoryGirl.create(:user) }
-
   before { @microposts = user.microposts.build(content: "Lorem ipsum") }
 
   subject { @microposts }
@@ -35,6 +34,16 @@ describe Micropost do
 
   describe "when user_id is not present" do
     before { @microposts.user_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe "with blank content" do
+    before { @microposts.content = " " }
+    it { should_not be_valid }
+  end
+
+  describe "with content that is too long" do
+    before { @microposts.content = "a" * 141 }
     it { should_not be_valid }
   end
 end
